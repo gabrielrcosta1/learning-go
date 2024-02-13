e6fcebd3b8e405ccf0eab726672043c4d5ac3104
gabrielrcosta1 <contatogrsoftwares@gmail.com>
2024-02-13 13:14:44 -0300

    create folder for api end init go mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gabrielrcosta1/learning-go
+
+go 1.22.0
--- static/main.go
+++ static/main.go
@@ -10,8 +10,8 @@ func main() {
 	http.HandleFunc("/code/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, you've requested")
 	})
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	fs := http.FileServer(http.Dir("template/"))
+	http.Handle("/template/", http.StripPrefix("/template/", fs))
 	http.ListenAndServe(":8080", nil)
 
 }
